380f98e5e2e35158052b283afa5617eeffaf6512
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2024-07-21 05:53:50 -0400

    Endpoint nuevas

---
+++ src/service/atracciones.js
@@ -0,0 +1,20 @@
+
+const API_URL = 'http://localhost:8080';
+
+const request = async (endpoint, options = {}) => {
+    const response = await fetch(`${API_URL}${endpoint}`, options);
+    if (!response.ok) {
+        throw new Error('Error en la solicitud');
+    }
+    return response.json();
+};
+
+export const getAllAtracciones = () => request('/attractions');
+// export const getItem = (id) => request(`/items/${id}`);
+// export const createItem = (data) => request('/items', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+// });
