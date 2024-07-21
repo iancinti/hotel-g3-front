fc5af99a99374fcd29f7a7c6cdbb408337614743
socheese <flor.0032@icloud.com>
2024-07-21 03:06:04 -0600

    Cambio de header, implementación de layout y page para atracciones

---
+++ src/app/atracciones/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+import * as React from 'react';
+import Carrousel from '../components/carrousel';
+import simple1 from '../../../public/images/gallery/simple01.jpg';
+import simple2 from '../../../public/images/gallery/simple02.jpg';
+import simple3 from '../../../public/images/gallery/simple03.jpg';
+import simple4 from '../../../public/images/gallery/simple04.jpg';
+import { Typography } from '@mui/material';
+
+
+function Gallery() {
+    const simple = [
+        { src: simple1 , alt: "Habitación", legend: "Habitación simple" },
+        { src: simple2, alt: "Habitación", legend: "Habitación simple" },
+        { src: simple3, alt: "Habitación", legend: "Habitación simple" },
+        { src: simple4, alt: "Habitación", legend: "Habitación simple" },
+      ];
+      const h1style = {
+        textAlign: 'center',
+        fontSize: '2.5em',
+        color: '#081928',
+        fontWeight: '700',
+        margin: '3em',
+      }
+      const h1style2 ={
+        ...h1style,
+        textAlign: 'left',
+      }
+    return(
+        <div >
+            <Typography style={h1style} >LUGARES TURÍSTICOS</Typography>            
+            <h1 style={h1style2}>SUITE SIMPLE</h1>
+            <Carrousel images={simple} />
+            
+        </div>
+    );
+}; 
+ 
+   
+
+export default Gallery;
--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -1,12 +1,14 @@
 import MenuComponent from "./menu";
 import './header.css'
 
-function Header({ bannerImg }) {
+function Header({ bannerImg, backgroundColor }) {
     const display = (bannerImg) ? 'block' : 'none'
 
     return(
         <header className="hd">
-            <div className="hd-banner">
+            <div className="hd-banner" style={{ backgroundColor: backgroundColor }}> 
+                
+                
                 <img
                     className="hd-banner-img"
                     src={bannerImg} alt="banner"
